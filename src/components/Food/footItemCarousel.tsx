7acea2df87fa75b18a2d764aaa095b1c2133226c
Ganbat <dev@erxes.io>
2025-08-11 18:09:26 +0800

    up

--- src/components/Food/footItemCarousel.tsx
+++ src/components/Food/footItemCarousel.tsx
@@ -1,13 +1,12 @@
-"use client";
 import React from "react";
 import { ICmsPost } from "@/src/graphql/types/cms.types";
 import Item from "./item";
 
 const FoodItemCarousel = ({ posts }: { posts: ICmsPost[] }) => {
   return (
-    <div className="px-4 md:px-8 py-12">
+    <div className="px-4 md:px-8 py-12 ">
       {/* Mobile & Tablet layout */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 lg:hidden">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 lg:hidden">
         {posts.map((post, index) => (
           <div key={index}>
             <Item post={post} />
@@ -15,14 +14,11 @@ const FoodItemCarousel = ({ posts }: { posts: ICmsPost[] }) => {
         ))}
       </div>
 
-      {/* Desktop horizontal scroll */}
+      {/* Desktop carousel layout */}
       <div className="hidden lg:block mt-12">
-        <div className="flex gap-8 overflow-x-auto scrollbar-hide px-4">
+        <div className="flex gap-48 justify-center">
           {posts.map((post, index) => (
-            <div
-              key={index}
-              className="min-w-[320px] max-w-[400px] flex-shrink-0"
-            >
+            <div key={index} className="w-[90vw] max-w-[500px] shrink-0 px-2">
               <Item post={post} />
             </div>
           ))}
